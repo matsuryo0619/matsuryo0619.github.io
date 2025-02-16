dd84a14d6a11663679776ca6afcff50416548799
matsuryo0619 <ryo.matsu0619@gmail.com>
2025-02-16 18:10:29 +0900

    Update myStuff.js

--- myStuff.js
+++ myStuff.js
@@ -4,8 +4,14 @@ fetch('myStuff.json')
     const jsonLength = data.length;
 
     for (let i = 0; i < jsonLength; i++) {
-      console.log(`Iteration ${i + 1}:`);
-      console.log(`ID: ${data[i].id}, Site: ${data[i].site}`);
+      fetch(`https://scratch.mit.edu/project/${data[i].id}/`)
+        .then(response => response.text())
+        .then(html => {
+          const parser = new DOMParser();
+          const doc = parser.parseFormString(html, "text/html");
+          const false_title = doc.title;
+          const title = false_title.slice(0, -11);
+      console.log(`タイトル: ${title}, Site: ${data[i].site}`);
     }
   })
   .catch(error => {
