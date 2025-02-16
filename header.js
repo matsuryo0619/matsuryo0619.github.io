45d0384984b5ba234b08a76b82555d5000788dab
matsuryo0619 <ryo.matsu0619@gmail.com>
2025-02-16 09:00:10 +0900

    Update header.js

--- header.js
+++ header.js
@@ -1,1 +1,9 @@
+//ヘッダーを作成
+const header = document.createElement('header');
+header.id = 'header';
+
+//ヘッダーの子要素を作成
+const Logo = document.createElement('img');
+Logo.id = 'header_logo';
+Logo.src = 'img/Logo.png';
 
