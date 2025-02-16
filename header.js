b40c18a03fd61cc195a5a7dd90b3952324cd2e8c
matsuryo0619 <ryo.matsu0619@gmail.com>
2025-02-16 09:21:33 +0900

    Update header.js

--- header.js
+++ header.js
@@ -7,3 +7,26 @@ const Logo = document.createElement('img');
 Logo.id = 'header_logo';
 Logo.src = 'img/Logo.png';
 
+const Search = document.createElement('input');
+Search.type = 'text';
+Search.id = 'header_Search';
+Search.placeholder = 'サイト内検索';
+
+//ボタンの親リスト
+const Button_parent = document.createElement('ul');
+Button_parent.style.listStyle = 'none';
+
+const ButtonList = [
+  {text: 'ホーム', src: 'Home.html'}
+]
+
+ButtonList.forEach((data) => {
+  const Button = document.createElement('button');
+  Button.classList.add('header_List');
+  Button.textContent = data.text;
+  Button.setAttribute('data-src', data.src);
+  Button_parent.appendChild(Button);
+});
+
+//ヘッダーに子要素を追加
+header.appendChild(Logo);
