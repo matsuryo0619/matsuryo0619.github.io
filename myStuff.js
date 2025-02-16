46da7df348a3c1a074362227528054dfae50feab
matsuryo0619 <ryo.matsu0619@gmail.com>
2025-02-16 17:59:51 +0900

    Update myStuff.js

--- myStuff.js
+++ myStuff.js
@@ -1,1 +1,13 @@
+fetch('myStuff.json')
+  .then(response => response.json())
+  .then(data => {
+    const jsonLength = data.length;
 
+    for (let i = 0; i < jsonLength; i++) {
+      console.log(`Iteration ${i + 1}:`);
+      console.log(`ID: ${data[i].id}, Site: ${data[i].site}`);
+    }
+  })
+  .catch(error => {
+    console.error('Error fetching JSON:', error);
+  });
