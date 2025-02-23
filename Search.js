869f241fd37ae817814d035d0d1289c071d035aa
matsuryo0619 <ryo.matsu0619@gmail.com>
2025-02-23 10:19:42 +0900

    Update Search.js

--- Search.js
+++ Search.js
@@ -1,7 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
   window.addEventListener('headerSearchCreated', function (event) {
     const searchInput = event.detail.searchInput;
-    const searchQuery = sessionStorage.getItem('searchQuery') || '';
+    const urlParams = new URLSearchParams(window.location.search);
+    const searchQuery = urlParams.get('q') || '';
     const resultList = document.getElementById('searchResults');
     searchInput.value = searchQuery;
     document.title = `${searchQuery} - スゴスク!`;
