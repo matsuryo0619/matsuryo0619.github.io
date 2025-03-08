26f4beedcce51835c0c22190e12f7051782ad0ad
matsuryo0619 <ryo.matsu0619@gmail.com>
2025-03-08 15:27:36 +0900

    Update footer.js

--- footer.js
+++ footer.js
@@ -1,8 +1,16 @@
-  //フッターの親要素を作成
-  const footer = document.createElement('footer');
-  footer.id = 'footer';
+//フッターの親要素を作成
+const footer = document.createElement('footer');
+footer.id = 'footer';
 
-  //フッターメニュー
+//フッターメニュー
 
-  //フッターを追加
-  document.body.appendChild(footer);
+//フッター矢印 - 上
+const footer_uparrow = document.createElement('i');
+footer_uparrow.classList.add('fa-regular');
+footer_uparrow.classList.add('fa-square-caret-up');
+
+//フッターにメニューを追加
+footer.appendChild(footer_uparrow);
+
+//フッターを追加
+document.body.appendChild(footer);
