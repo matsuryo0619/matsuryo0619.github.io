a269c8dad1de701105fa26ebaad1e4832ee4ef89
matsuryo0619 <ryo.matsu0619@gmail.com>
2025-03-08 17:31:46 +0900

    Update footer.js

--- footer.js
+++ footer.js
@@ -9,9 +9,21 @@ const footer_uparrow = document.createElement('i');
 footer_uparrow.classList.add('fa-regular');
 footer_uparrow.classList.add('fa-square-caret-up');
 footer_uparrow.id = 'footer_uparrow';
+//フッター矢印 - 下
+const footer_downarrow = document.createElement('i');
+footer_downarrow.classList.add('fa-regular');
+footer_downarrow.classList.add('fa-square-caret-down');
+footer_downarrow.id = 'footer_downarrow';
 
 //フッターにメニューを追加
 footer.appendChild(footer_uparrow);
+footer.appendChild(footer_downarrow);
 
 //フッターを追加
 document.body.appendChild(footer);
+
+//フッター処理系
+footer.addEventListener('mouseenter', () => {
+  footer_uparrow.style.opacity = '0';
+  footer_downarrow.style.opacity = '1';
+});
