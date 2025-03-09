9fa1932558600b96f5818eafdc3269a32473070b
matsuryo0619 <ryo.matsu0619@gmail.com>
2025-03-09 21:40:02 +0900

    Update main.js

--- main.js
+++ main.js
@@ -8,16 +8,10 @@ setTimeout(function() {
     }
 }, 1000);
 
-// 新しい rand を作成
-let rand = Math.floor(Math.random() * 1000000);
-rand = String(rand).padStart(6, '0');
-
-// 現在のURLパラメータを取得
-let urlParams = new URLSearchParams(window.location.search);
-
-// `rand` を削除してから新しい値をセット（これで二重にならない！）
-urlParams.delete('rand');
-urlParams.set('rand', rand);
-
-// 更新されたURLにリダイレクト
-window.location.href = `${window.location.pathname}?${urlParams.toString()}`;
+if (!window.location.search.includes('rand=')) {  
+    let urlParams = new URLSearchParams(window.location.search); // `if` の中だけで宣言
+    let rand = Math.floor(Math.random() * 1000000);
+    rand = String(rand).padStart(6, '0');
+    urlParams.set('rand', rand);
+    window.location.href = `${window.location.pathname}?${urlParams.toString()}`;
+}
