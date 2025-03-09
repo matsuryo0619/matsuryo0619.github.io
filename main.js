c723d3c11003cc26010cc148e0a030578066b3cb
matsuryo0619 <ryo.matsu0619@gmail.com>
2025-03-09 21:46:10 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,1 @@
-//ヘッダー･フッターが無い場合
-setTimeout(function() {
-    const header = document.getElementById('header');  
-    const footer = document.getElementById('footer');  
 
-    if (!header || !footer) {
-        location.reload();
-    }
-}, 1000);
-
-if (!window.location.search.includes('rand=')) {  
-    let urlParams = new URLSearchParams(window.location.search); // `if` の中だけで宣言
-    let rand = Math.floor(Math.random() * 1000000);
-    rand = String(rand).padStart(6, '0');
-    urlParams.set('rand', rand);
-    window.location.href = `${window.location.pathname}?${urlParams.toString()}`;
-}
