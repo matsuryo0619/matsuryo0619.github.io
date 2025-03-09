2768291fd9d90fde2b0033f453bfbc656a017844
matsuryo0619 <ryo.matsu0619@gmail.com>
2025-03-09 11:39:44 +0900

    Update myStuff.js

--- myStuff.js
+++ myStuff.js
@@ -25,7 +25,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
       projectDiv.innerHTML = `
         <div class="project-header">
-          <h3><a href="https://scratch.mit.edu/projects/${item.id}/" target="_blank" class="preview-link">${item.text}</a></h3>
+          <h3><a href="https://matsuryo0619.github.io/scratchblog/link.html?link=${encodeURIComponent('https://scratch.mit.edu/projects/'${item.id})}" target="_blank" class="preview-link">${item.text}</a></h3>
           <span class="user-name" data-user="${item.user || '不明'}">${item.user || '不明'}</span>
         </div>
         <p class="project-content">${item.content || '説明なし'}</p>
