f91a7058f3174af1d09c100de9291c600436d5b7
matsuryo0619 <ryo.matsu0619@gmail.com>
2025-03-09 21:37:33 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,20 @@
-//ヘッダー･フッターがなかった場合
+//ヘッダー･フッターが無い場合
 setTimeout(function() {
-    const header = document.getElementById('header');  // 例: IDがelement1の要素
-    const footer = document.getElementById('footer');  // 例: IDがelement2の要素
+    const header = document.getElementById('header');  
+    const footer = document.getElementById('footer');  
 
     if (!header || !footer) {
-      //どちらかがなかったらページをリロード
-      location.reload();  
+        location.reload();
     }
-}, 1000); // 1000ms = 1秒
+}, 1000);
 
-//常にデータを更新させるように
-if (!window.location.search.includes('rand=')) {  
-    let urlParams = new URLSearchParams(window.location.search); // `if` の中だけで宣言
-    let rand = Math.floor(Math.random() * 1000000);
-    rand = String(rand).padStart(6, '0');
-    urlParams.set('rand', rand);
-    window.location.href = `${window.location.pathname}?${urlParams.toString()}`;
-}
+// 新しい rand を作成
+let rand = Math.floor(Math.random() * 1000000);
+rand = String(rand).padStart(6, '0');
+
+// 現在のURLパラメータを取得
+let urlParams = new URLSearchParams(window.location.search);
+
+// `rand` を毎回更新
+urlParams.set('rand', rand);
+window.location.href = `${window.location.pathname}?${urlParams.toString()}`;
