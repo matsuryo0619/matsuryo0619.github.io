74a850073ad33e9de204fc2817d85aea9bfc247f
matsuryo0619 <ryo.matsu0619@gmail.com>
2025-07-18 21:39:14 +0900

    Update context.js

--- context.js
+++ context.js
@@ -8,21 +8,18 @@ menu.style.display = 'none';
 
 document.body.appendChild(menu);
 
-// 右クリックのブラウザメニューを消す
-document.oncontextmenu = function () {
-  return false;
-};
+document.oncontextmenu = () => false;
 
 document.addEventListener('contextmenu', (event) => {
-  event.preventDefault(); // これも入れとくと安心
+  event.preventDefault();
 
-  if (menu.contains(event.target)) {
-    // メニューの中を右クリックしたら位置変えて表示
-    menu.style.left = `${event.clientX}px`;
-    menu.style.top = `${event.clientY}px`;
-    menu.style.display = 'block';
-  } else {
-    // メニュー外を右クリックしたら非表示
+  menu.style.left = `${event.clientX}px`;
+  menu.style.top = `${event.clientY}px`;
+  menu.style.display = 'block';
+});
+
+document.addEventListener('click', (event) => {
+  if (!menu.contains(event.target)) {
     menu.style.display = 'none';
   }
 });
