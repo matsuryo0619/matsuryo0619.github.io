498cef2e62f6f59aff13b1119da612125a884642
matsuryo0619 <ryo.matsu0619@gmail.com>
2025-07-18 21:00:45 +0900

    Update context.js

--- context.js
+++ context.js
@@ -1,9 +1,12 @@
 const menu = document.createElement('div');
 menu.id = 'context';
+menu.classList.add('border');
 
 const menus = [
   {type: 'btn', text: ''}
 ];
+
+document.body.appendChild(menu);
 
 document.oncontextmenu = function () {
   return false;
