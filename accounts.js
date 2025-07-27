26b5e47a651c18a0d52b409617653e3b70722aa1
matsuryo0619 <ryo.matsu0619@gmail.com>
2025-07-28 00:36:32 +0900

    Update accounts.js

--- accounts.js
+++ accounts.js
@@ -1,15 +1,18 @@
 window.addEventListener('headerSearchCreated', async () => {
   const { accounts_parent } = await import('./header.js');
   const Make_accounts = document.getElementById('header_Toaccounts');
-  const sign_in = document.getElementById('header_ToSignin');
+  const login = document.getElementById('header_Tologin');
+  const urlParams = new URLSearchParams(window.location.search);
   if (!localStorage.getItem('account')) {
     accounts_parent.style.display = 'none';
     if (window.location.origin + window.location.pathname === 'https://matsuryo0619.github.io/scratchblog/accounts.html') {
+      if (urlParams.get('type') === 'make') {
       document.title = 'アカウントを作成 - スゴスク!';
-    }
+      } else if (urlParams.get('type') === 'login') {
+        document.title = 'ログイン - スゴスク!';
   } else {
     Make_accounts.style.display = 'none';
-    sign_in.style.display = 'none';
+    login.style.display = 'none';
     accounts_parent.querySelector('p').textContent = localStorage.getItem('account');
     if (window.location.origin + window.location.pathname === 'https://matsuryo0619.github.io/scratchblog/accounts.html') {
       document.title = 'アカウント設定 - スゴスク!';
