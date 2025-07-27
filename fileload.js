bb065c630e37c36a41124547a89ca3a4f799b78e
matsuryo0619 <ryo.matsu0619@gmail.com>
2025-07-27 17:27:34 +0900

    Update fileload.js

--- fileload.js
+++ fileload.js
@@ -14,7 +14,8 @@ const reqjs = [
   {src: 'context', defer: true},
   {src: 'footer', defer: true},
   {src: 'main', defer: false},
-  {src: 'https://cdn.jsdelivr.net/npm/marked/marked.min.js', defer: true}
+  {src: 'https://cdn.jsdelivr.net/npm/marked/marked.min.js', defer: true},
+  {src: 'https://cdn.jsdelivr.net/npm/dompurify@2.4.0/dist/purify.min.js', defer: true}
 ];
 
 // CSSを読み込む
