3c32568a4dd7bc51b669ed16ba4127e8ed9fb2ff
matsuryo0619 <ryo.matsu0619@gmail.com>
2025-07-27 21:27:36 +0900

    Update accounts.js

--- accounts.js
+++ accounts.js
@@ -1,1 +1,5 @@
-
+window.addEventListener('headerSearchCreated', () => {
+  import { accounts_parent } from './header.js';
+  if (!localStorage.getItem('account')) {
+    accounts_parent.style.display = 'none';
+});
