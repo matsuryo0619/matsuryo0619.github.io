ff11fee4f476d30558b75e5aea1500df28a8e89a
matsuryo0619 <ryo.matsu0619@gmail.com>
2025-07-28 11:55:57 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,3 @@
-const originalClear = secureAuth.clearAuthData.bind(secureAuth);
-secureAuth.clearAuthData = function(...args) {
-  alert('clearAuthData() called!', new Error().stack);
-  return originalClear(...args);
-}
-
 //ヘッダー･フッターが無い場合
 setTimeout(function() {
     const header = document.getElementById('header');  
